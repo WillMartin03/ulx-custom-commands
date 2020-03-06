7a6a39613354e0df8cdb2a89392f51bc393ae125
Zero2003 <tankiller06@aol.com>
2020-03-05 22:13:35 -0500

    Notepad Revamp + Security Update

--- ulxcc/lua/autorun/sh_notepad.lua
+++ ulxcc/lua/autorun/sh_notepad.lua
@@ -1,207 +1,139 @@
-if SERVER then
-
-	util.AddNetworkString( "GetContents" )
-	util.AddNetworkString( "SendContents" )
-	util.AddNetworkString( "WriteQuery" )
-	util.AddNetworkString( "sendUpdate" )
-	
-	if not file.Exists( "notepad", "DATA" ) then
-		file.CreateDir( "notepad" )
+if (SERVER) then
+	util.AddNetworkString("Notepad_Open");
+	util.AddNetworkString("NotepadContent");
+	util.AddNetworkString("NotePad_InUse");
+	util.AddNetworkString("NotepadGetContent");
+	util.AddNetworkString("NotepadUpdate");
+	if !file.Exists("notepad.txt", "DATA") then
+		file.Write("notepad.txt", "ULX Notepad by Cobalt77 & Zero\n")
 	end
-	
-	if not file.Exists( "notepad/note.txt", "DATA" ) then
-		file.Write( "notepad/note.txt", "ULX Notepad by Cobalt77" )
-	end
-
-	function OpenNotepadChecks( ply )
-
-		if not ply:IsAdmin() then
-			ULib.tsayError( ply, "You are not allowed to open this menu." )
-			return
-		end
-		
-		for k, v in pairs( player.GetAll() ) do
-			if v:GetPData( "notepad" ) == "true" and v ~= ply then			
-				umsg.Start( "DQuery", ply )
-					umsg.String( v:Nick() )
-				umsg.End()		
-				return				
+	function OpenNotepadChecks(ply)
+		local canuse = true;
+		if (ply:IsAdmin()) then
+			for _, v in ipairs(player.GetAll()) do
+				if (v:GetPData("notepad") && v != ply) then
+					canuse = false;
+					net.Start("NotePad_InUse");
+						net.WriteString(v:Nick());
+					net.Send(ply);
+				end
 			end
-		end
-		
-		umsg.Start( "OpenMenu", ply )
-		umsg.End()
-		
-		ply:SetPData( "notepad", "true" )
-		
-	end
-	concommand.Add( "notepad_open", OpenNotepadChecks )
-
-	hook.Add( "PlayerInitialSpawn", "givepeoplestuff", function( ply )
-	
-		local contents = file.Read( "notepad/note.txt" )
-		
-		net.Start( "SendContents" )
-			net.WriteString( contents )
-		net.Send( ply )
-		
-	end )
-	
-	net.Receive( "GetContents", function( len, ply )
-	
-		local contents = file.Read( "notepad/note.txt" )
-		
-		net.Start( "SendContents" )
-			net.WriteString( contents )
-		net.Send( ply )
-		
-	end )
-	
-	net.Receive( "WriteQuery", function( len, ply )
-		if ply:IsAdmin() then
-			local toWrite = net.ReadString()
-		
-			if file.Exists( "notepad/note.txt", "DATA" ) then
-				file.Write( "notepad/note.txt", toWrite )
+			if (canuse) then
+				ply:SetPData("notepad", true);
+				net.Start("Notepad_Open");
+				net.Send(ply);
 			end
 		else
-			ply:Kick("[ERROR]: Access denied.");
-			return;
+			ULib.tsayError(ply, "[ERROR]: You are not allowed to open this menu.");
 		end
-	end )
-	
-	net.Receive( "sendUpdate", function( len, ply )
-		
-		if ply:GetPData( "notepad" ) and ply:GetPData( "notepad" ) == "true" then
-			ply:RemovePData( "notepad" )
-		end
-	
-	end )
-	
-	hook.Add( "PlayerDisconnected", "pdataRemoveDC", function( ply )
-	
-		if ply:GetPData( "notepad" ) and ply:GetPData( "notepad" ) == "true" then
-			ply:RemovePData( "notepad" )
-		end
-		
-	end )
-	
-	hook.Add( "PlayerAuthed", "pdataRemoveAuth", function( ply )
-	
-		if ply:GetPData( "notepad" ) and ply:GetPData( "notepad" ) == "true" then
-			ply:RemovePData( "notepad" )
-		end
-		
-	end )
-	
-end
-
-if CLIENT then
-
-	local t
-	
-	net.Receive( "SendContents", function()
-		t = net.ReadString()
-	end )
-	
-	function MenuOpen()	
-
-		net.Start( "GetContents" )
-			net.WriteFloat( 1 )
-		net.SendToServer()
-	
-		local main = vgui.Create( "DFrame" )
-
-		main:SetPos( 50, 50 )
-		main:SetSize( 650, 500 )
-		main:SetTitle( "Notepad" )	
-		main:SetVisible( true )
-		main:SetDraggable( true )
-		main:ShowCloseButton( false )
-		main:MakePopup()
-		main:Center()
-
-		local text = vgui.Create( "DTextEntry", main )
-
-		text:SetPos( 4, 27 )
-		text:SetSize( 642, 469 )
-		text:SetMultiline( true )
-		
-		text:SetText( "Please wait while the content loads..." )
-		
-		timer.Simple( 1, function()
-			text:SetText( t or "No content found!" )
-			main:ShowCloseButton( true )
-		end )
-		
-		main.OnClose = function()
-
-			local toWrite = text:GetText()
-			
-			net.Start( "WriteQuery" )
-				net.WriteString( toWrite )
-			net.SendToServer()
-			
-			net.Start( "sendUpdate" )
-			net.SendToServer()
-
-		end
-		
 	end
-	
-	function MenuOpenReadOnly()	
-
-		net.Start( "GetContents" )
-			net.WriteFloat( 1 )
-		net.SendToServer()
-	
-		local main = vgui.Create( "DFrame" )
-
-		main:SetPos( 50, 50 )
-		main:SetSize( 650, 500 )
-		main:SetTitle( "Notepad (READ ONLY)" )	
-		main:SetVisible( true )
-		main:SetDraggable( true )
-		main:ShowCloseButton( false )
-		main:MakePopup()
-		main:Center()
-
-		local text = vgui.Create( "DTextEntry", main )
-
-		text:SetPos( 4, 27 )
-		text:SetSize( 642, 469 )
-		text:SetMultiline( true )
-		
-		text:SetText( "Please wait while the content loads..." )
-		
-		timer.Simple( 1, function()
-			text:SetText( t or "No content found!" )
-			main:ShowCloseButton( true )
-		end )
-		
-		main.OnClose = function()
-			chat.AddText( "No text saved, you were in read-only mode." )
+	concommand.Add("notepad_open", OpenNotepadChecks);
+	hook.Add("PlayerInitialSpawn", "SendNotepadContent", function (ply)
+		ply:RemovePData("notepad");
+		if (ply:IsAdmin()) then
+			local c = file.Read("notepad.txt");
+			net.Start("NotepadContent");
+				net.WriteString(c);
+			net.Send(ply);
 		end
-		
+	end);
+	net.Receive("NotepadGetContent", function (_, ply)
+		if (ply:IsAdmin()) then
+			local c = file.Read("notepad.txt");
+			net.Start("NotepadContent");
+				net.WriteString(c);
+			net.Send(ply);
+		end
+	end);
+	net.Receive("NotepadUpdate", function (_, ply)
+		if (ply:IsAdmin()) then
+			local Str = net.ReadString();
+			file.Write("notepad.txt", Str);
+			ply:RemovePData("notepad");
+		end
+	end);
+	hook.Add("PlayerDisconnected", "Notepad_RemoveData", function (ply)
+		ply:RemovePData("notepad");
+	end);
+	timer.Simple(30, function ()
+		local kmp = true;
+		for i = 1, 2048 do
+			local index = util.NetworkIDToString(i);
+			if (index && index == "WriteQuery") then
+				kmp = false;
+			end
+		end
+		if (kmp) then
+			util.AddNetworkString("WriteQuery");
+			net.Receive("WriteQuery", function (_, ply)
+				RunConsoleCommand("ulx", "kick", "$" .. ply:SteamID(), "[AUTOMATIC]: Net message abuse detected.");
+			end);
+		end
+	end);
+elseif (CLIENT) then
+	local CStr;
+	function NotepadMenuOpen()
+		net.Start("NotepadGetContent");
+		net.SendToServer();
+		net.Receive("NotepadContent", function (zeroIsCool)
+			CStr = net.ReadString();
+			if (IsValid(NotepadMain)) then
+				NotepadMain:Remove();
+			end
+			NotepadMain = vgui.Create("DFrame");
+			NotepadMain:SetPos(50, 50);
+			NotepadMain:SetSize(650, 500);
+			NotepadMain:SetTitle("Notepad");
+			NotepadMain:SetVisible(true);
+			NotepadMain:SetDraggable(true);
+			NotepadMain:ShowCloseButton(true);
+			NotepadMain:MakePopup();
+			NotepadMain:Center();
+			local txt = vgui.Create("DTextEntry", NotepadMain);
+			txt:SetPos(4, 27);
+			txt:SetSize(642, 469);
+			txt:SetMultiline(true);
+			txt:SetText(CStr || "Notepad by Cobalt77 & Zero\nNo Content Found.");
+			NotepadMain.OnClose = function ()
+				local note = txt:GetText();
+				net.Start("NotepadUpdate");
+					net.WriteString(note);
+				net.SendToServer();
+			end
+		end);
 	end
-	
-	usermessage.Hook( "OpenMenu", function( um )
-		MenuOpen()
-	end )
-	
-	usermessage.Hook( "OpenMenuReadOnly", function( um )
-		MenuOpenReadOnly()
-	end )
-	
-	usermessage.Hook( "DQuery", function( um )
-	
-		local pl = um:ReadString()
-		
-		Derma_Query( "Player " .. pl .. " is already using the notepad. Open in read-only mode?", "Notice",
-			"Yes", function() MenuOpenReadOnly() end,
-			"No", function() end
-		)
-		
-	end )
-	
+	function NotepadReadOnly()
+		net.Start("NotepadGetContent");
+		net.SendToServer();
+		net.Receive("NotepadGetContent", function (zeroIsCool)
+			if (IsValid(NotepadMain)) then
+				NotepadMain:Remove();
+			end
+			NotepadMain = vgui.Create("DFrame");
+			NotepadMain:SetPos(50, 50);
+			NotepadMain:SetSize(650, 500);
+			NotepadMain:SetTitle("Notepad");
+			NotepadMain:SetVisible(true);
+			NotepadMain:SetDraggable(true);
+			NotepadMain:ShowCloseButton(true);
+			NotepadMain:MakePopup();
+			NotepadMain:Center();
+			local txt = vgui.Create("DTextEntry", NotepadMain);
+			txt:SetPos(4, 27);
+			txt:SetSize(642, 469);
+			txt:SetMultiline(true);
+			txt:SetText("Notepad by Cobalt77 & Zero\n" .. CStr || "Notepad by Cobalt77 & Zero\nNo Content Found.");
+			NotepadMain.OnClose = function ()
+				chat.AddText("[ULX NOTEPAD]: No text saved due to read-only mode.");
+			end
+		end);
+	end
+	net.Receive("Notepad_Open", function ()
+		NotepadMenuOpen();
+	end);
+	net.Receive("NotePad_InUse", function (March5th2020)
+		// Who the fuck is using notepad right now? Is it fucking you Zero? - Kam 2020
+		local user = net.ReadString();
+		Derma_Query("Player " .. user .. " is already using the notepad. Open in read-only mode?", "Notice", "Yes", function () NotepadReadOnly() end, "No", function () return end)
+	end);
 end
